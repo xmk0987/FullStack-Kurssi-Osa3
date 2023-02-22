0af10903ac89ff712e9c0adaec8c366a98f87b89
ftonvi <onni.vitikainen@tuni.fi>
2023-02-22 02:26:11 +0200

    step3

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ const app = express()
 
 app.use(express.json())
 
-__dirname = "FullStack-osa3"
 
 let persons = [
     {
@@ -39,6 +38,17 @@ app.get('/api/persons', (request, response) => {
     response.json(persons)
 })
 
+
+app.get('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+    const person = persons.find(person => person.id === id)
+
+    if (person) {
+        response.json(person)
+    } else {
+        response.status(404).end()
+    }
+})
 
 const PORT = 3001
 app.listen(PORT, () => {
