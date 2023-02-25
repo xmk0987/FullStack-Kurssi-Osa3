bada4953431de49589312b010cf861f05262b4c9
ftonvi <onni.vitikainen@tuni.fi>
2023-02-25 23:25:56 +0200

    Added mongo Database, 3.13-3.14

--- index.js
+++ index.js
@@ -1,40 +1,28 @@
 const express = require('express')
-const morgan = require('morgan')
 const app = express()
 const cors = require('cors')
-app.use(express.static('build'))
+require('dotenv').config()
+
+const Person = require('./models/person')
+
+const requestLogger = (request, response, next) => {
+  console.log('Method:', request.method)
+  console.log('Path:  ', request.path)
+  console.log('Body:  ', request.body)
+  console.log('---')
+  next()
+}
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+
 app.use(cors())
-
-morgan.token('body', (req, res) => JSON.stringify(req.body));
-  
 app.use(express.json())
-app.use(morgan(':method :url :status :response-time ms :body '));
-
-
-
-
+app.use(requestLogger)
+app.use(express.static('build'))
 
 let persons = [
-    {
-        id: 1,
-        name: "Arto Hellas",
-        number: "040-123456"
-    },
-    {
-        id:2,
-        name: "Ada Lovelace",
-        number: "39-44-5323523"
-    },
-    {
-        id:3,
-        name: "Dan Abramov",
-        number: "12-43-234345"
-    },
-    {
-        id:4,
-        name: "Mary Poppendick",
-        number: "39-23-6423122"
-    }
 ]
 
 app.get('/info', (request, response) => {
@@ -45,19 +33,16 @@ app.get('/info', (request, response) => {
   })
 
 app.get('/api/persons', (request, response) => {
-    response.json(persons)
+    Person.find({}).then(persons => {
+        response.json(persons)
+    })
 })
 
 
 app.get('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id)
-    const person = persons.find(person => person.id === id)
-
-    if (person) {
-        response.json(person)
-    } else {
-        response.status(404).end()
-    }
+    Person.findById(request.params.id).then(note => {
+        response.json(note)
+    })
 })
 
 
@@ -70,14 +55,31 @@ app.delete('/api/persons/:id', (request, response) => {
     response.status(204).end()
 })
 
-
+/*
 const generateId = () => {
     const newId = Math.floor(Math.random() * (9999999999999-1) + 1)
     
     return newId
   }
+*/
 
-
+app.post('/api/persons', (request, response) => {
+    const body = request.body
+  
+    if (body.name === undefined) {
+      return response.status(400).json({ error: 'content missing' })
+    }
+  
+    const person = new Person({
+      name: body.name,
+      number: body.number || false,
+    })
+  
+    person.save().then(savedNote => {
+      response.json(savedNote)
+    })
+  })
+/*
 app.post('/api/persons', (request, response) => {
     const newperson = request.body
 
@@ -91,23 +93,27 @@ app.post('/api/persons', (request, response) => {
             error: 'Name or number missing' 
           })
     }
+    if (newperson.name === undefined) {
+        return response.status(400).json({ error: 'content missing' })
+      }
 
     const person = {
-        id: generateId(),
+        //id: generateId(),
         name: newperson.name,
         number: newperson.number
         
     }
-    persons = persons.concat(person)
 
-    console.log(newperson)
+    console.log("tulee tänne")
+    person.save().then(savedPerson => {
+        response.json(savedPerson)
+    })
     
+})*/
 
-    response.json(newperson)
-})
+app.use(unknownEndpoint)
 
-
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT
 app.listen(PORT, () => {
-console.log(`Server running on port ${PORT}`)
+  console.log(`Server running on port ${PORT}`)
 })
