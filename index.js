256991e542013b9b7c6a23ae36aa418e030b553a
ftonvi <onni.vitikainen@tuni.fi>
2023-02-26 05:05:33 +0200

    3.19 added errorhandling

--- index.js
+++ index.js
@@ -57,12 +57,8 @@ app.get('/api/persons/:id', (request, response, next) => {
     .catch(error => next(error))
 })
 
-app.post('/api/persons', (request, response) => {
+app.post('/api/persons', (request, response, next) => {
     const body = request.body
-  
-    if (body.name === "") {
-      return response.status(400).json({ error: 'content missing' })
-    }
   
     const person = new Person({
       name: body.name,
@@ -72,6 +68,7 @@ app.post('/api/persons', (request, response) => {
     person.save().then(savedPerson => {
       response.json(savedPerson)
     })
+    .catch(error => next(error)) 
   })
 
 
@@ -149,6 +146,8 @@ const errorHandler = (error, request, response, next) => {
   
     if (error.name === 'CastError') {
       return response.status(400).send({ error: 'malformatted id' })
+    } else if (error.name === 'ValidationError') {
+        return response.status(400).json({ error: error.message })
     }
   
     next(error)
