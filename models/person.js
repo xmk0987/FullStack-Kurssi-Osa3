f6e6760adcadc414b07e419d70c8f00e63ac3d02
ftonvi <onni.vitikainen@tuni.fi>
2023-02-26 05:59:36 +0200

    3.20 ja 3.21 valmis versio

--- models/person.js
+++ models/person.js
@@ -20,7 +20,16 @@ mongoose.connect(url)
         minlength: 3,
         required:true
     },
-    number: String,
+    number: {
+        type: String,
+        minlength: 8,
+        validate: {
+            validator: function(v){
+                return /\d{2,3}-\d*/.test(v)
+            },
+            message: props => `${props.value} is not a valid phone number!`
+        } 
+    }
   })
 
 personSchema.set('toJSON', {
